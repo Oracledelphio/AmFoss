859d4bf3f8602fa176e4dfa941a700b4e77aef12
Oracledelphio <kurtsony16@gmail.com>
2023-09-12 10:16:34 +0530

    Adding Task

---
+++ task-08/script.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const apiKey = "1b80fb913eb0ecf9f28e11b0902b41eb"; // Replace with your API key
+    const weatherInfo = document.getElementById("weatherInfo");
+    const searchButton = document.getElementById("searchButton");
+
+    searchButton.addEventListener("click", () => {
+        const locationInput = document.getElementById("locationInput").value;
+
+        if (locationInput) {
+            fetchWeatherData(locationInput, apiKey);
+        } else {
+            alert("Please enter a location.");
+        }
+    });
+
+    function fetchWeatherData(location, apiKey) {
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
+
+        fetch(apiUrl)
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.cod === 200) {
+                    const temperature = data.main.temp;
+                    const description = data.weather[0].description;
+
+                    const weatherHTML = `
+                        <p>Location: ${location}</p>
+                        <p>Temperature: ${temperature}°C</p>
+                        <p>Description: ${description}</p>
+                    `;
+
+                    weatherInfo.innerHTML = weatherHTML;
+                } else {
+                    weatherInfo.innerHTML = `<p>Location not found. Please try again.</p>`;
+                }
+            })
+            .catch((error) => {
+                console.error("Error fetching weather data:", error);
+                weatherInfo.innerHTML = `<p>An error occurred. Please try again later.</p>`;
+            });
+    }
+});
